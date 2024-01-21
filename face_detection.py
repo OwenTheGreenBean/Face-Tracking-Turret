268462b56484a2ce3f8591f33ca3bf9e1f031149
Owen O'Brien <148368718+OwenTheGreenBean@users.noreply.github.com>
2024-01-21 14:27:42 -0500

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,32 @@
+import serial
+import time
+import numpy as np
+import cv2
+
+arduinoData = serial.Serial('/dev/cu.usbserial-110', 9600) #Change this based on your serial port
+
+def send_coordinates_to_arduino(x, y, w, h):
+    # Convert the coordinates to a string and send it to Arduino
+    coordinates = f"{x},{y}\r"
+    arduinoData.write(coordinates.encode())
+    print(f"X{x}Y{y}\n")
+
+capture = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    isTrue, frame = capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.05, 8, minSize=(120,120))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+        send_coordinates_to_arduino(x, y, w, h)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
